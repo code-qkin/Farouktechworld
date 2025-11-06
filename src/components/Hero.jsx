43dd6acbb0c0552bd1c2a2af2982a597a06d1c20
Apple <apple@Apples-MacBook-Pro.local>
2025-11-06 14:29:33 +0100

    overflow issue fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import heroBg from '../assets/images/howwework.png'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <>
@@ -8,15 +8,16 @@ const Hero = () => {
         <div className="relative z-10 h-screen bg-black bg-opacity-60 flex items-center justify-center">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
             <div className="text-left col-span-2 text-white px-4">
-              <p className="text-6xl font-bold mb-2 text-purple-400">
+              <p className="text-4xl md:text-6xl font-bold mb-4 text-purple-400">
                 Welcome to FaroukTechWorld Limited
               </p>
-              <p className="text-lg text-2xl text-purple-400 mb-2">
+              <p className="text-xl md:text-2xl text-purple-400 mb-4">
                 Don't Delay, Repair Today.
               </p>
-              <p className="text-lg text-2xl text-purple-400 mb-2">
+              <p className="text-base md:text-lg text-purple-400 mb-6 max-w-3xl break-words">
                 Don't let technical troubles slow you down. At FaroukTechWorld, we're here to breathe new life into your devices, swiftly & seamlessly.
               </p>
+
               <Link to="/pricing">
                 <button className="bg-purple-800 text-white px-6 py-3 rounded hover:bg-purple-700 transition">
                   Get a quote today
